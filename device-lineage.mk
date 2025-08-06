108d57cda704c23d230a5764fd902ecf3f29b8da
Austin Young <austineyoung2000@gmail.com>
2025-08-06 10:31:39 -0400

    akita: PixelSupport
    The change adds the PixelSupport and includeds in build time if present.

--- device-lineage.mk
+++ device-lineage.mk
@@ -25,6 +25,9 @@ PRODUCT_PACKAGES += \
 # PixelParts
 include packages/apps/PixelParts/device.mk
 
+# PixelSupport
+-include device/google/gs-common/pixelsupport/pixelsupport.mk
+
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
 
